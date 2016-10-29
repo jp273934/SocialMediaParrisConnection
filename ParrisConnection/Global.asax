cd3a2a92df9451673655b0267b2c387da3f4e4ad
Andrea and Jeremy <Andrea and Jeremy@GAMEMACHINE>
2016-10-28 19:04:34 -0500

    Add project files.

---
+++ ParrisConnection/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ParrisConnection.MvcApplication" Language="C#" %>
---
+++ ParrisConnection/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ParrisConnection.Startup))]
+namespace ParrisConnection
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
