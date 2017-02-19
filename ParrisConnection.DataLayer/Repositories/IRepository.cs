75b811fdcd12637544b3b6b825efce277f66cda6
Andrea and Jeremy <jparris195@gmail.com>
2017-02-19 16:37:40 -0600

    Added generic repository to datalayer

---
+++ ParrisConnection.DataLayer/Repositories/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace ParrisConnection.DataLayer.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+        void Update(T entityToUpdate);
+        void Insert(T entity);
+    }
+}
---
+++ ParrisConnection.DataLayer/Repositories/Repository.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace ParrisConnection.DataLayer.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        internal DbContext Context;
+        internal DbSet<T> DbSet;
+
+        public Repository(DbContext context)
+        {
+            Context = context;
+            DbSet   = context.Set<T>();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return DbSet.ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return DbSet.Find(id);
+        }
+
+        public void Update(T entityToUpdate)
+        {
+            DbSet.Attach(entityToUpdate);
+            Context.Entry(entityToUpdate).State = EntityState.Modified;
+            Context.SaveChanges();
+        }
+
+        public void Insert(T entity)
+        {
+            DbSet.Add(entity);
+            Context.SaveChanges();
+        }
+    }
+}
