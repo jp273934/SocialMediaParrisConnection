30778c6c88aaff185778baba996ab484dcff0030
Andrea and Jeremy <jparris195@gmail.com>
2017-04-22 21:45:11 -0500

    Automapped education data

--- ParrisConnection.ServiceLayer/Services/EducationService.cs
+++ ParrisConnection.ServiceLayer/Services/EducationService.cs
@@ -1,9 +1,9 @@
-﻿using ParrisConnection.DataLayer.DataAccess;
+﻿using AutoMapper;
+using ParrisConnection.DataLayer.DataAccess;
 using ParrisConnection.DataLayer.Entities.Profile;
 using ParrisConnection.ServiceLayer.Data;
 using ParrisConnection.ServiceLayer.Services.Interfaces;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace ParrisConnection.ServiceLayer.Services
 {
@@ -14,48 +14,22 @@ namespace ParrisConnection.ServiceLayer.Services
         public EducationService(IDataAccess dataAccess)
         {
             _dataAccess = dataAccess;
+            Mapper.Initialize(m => m.CreateMap<Education, EducationData>().ReverseMap());
         }
 
         public IEnumerable<EducationData> GetAllEducation()
         {
-            return _dataAccess.Educations.GetAll().Select(LoadData);
+            return Mapper.Map<IEnumerable<EducationData>>(_dataAccess.Educations.GetAll());
         }
 
         public EducationData GetEducationById(int id)
         {
-            return LoadData(_dataAccess.Educations.GetById(id));
+            return Mapper.Map<EducationData>(_dataAccess.Educations.GetById(id));
         }
 
         public void SaveEducation(EducationData education)
         {
-            _dataAccess.Educations.Insert(ConvertToEntity(education));
-        }
-
-
-        private EducationData LoadData(Education education)
-        {
-            return new EducationData
-            {
-                Id        = education.Id,
-                UserId    = education.UserId,
-                Name      = education.Name,
-                Degree    = education.Degree,
-                StartDate = education.StartDate,
-                EndDate   = education.EndDate
-            };
-        }
-
-        private Education ConvertToEntity(EducationData education)
-        {
-            return new Education
-            {
-                Id        = education.Id,
-                UserId    = education.UserId,
-                Name      = education.Name,
-                Degree    = education.Degree,
-                StartDate = education.StartDate,
-                EndDate   = education.EndDate
-            };
+            _dataAccess.Educations.Insert(Mapper.Map<Education>(education));
         }
     }
 }
