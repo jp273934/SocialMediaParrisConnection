8a65a23bf5d9173cc25698dffe2b4c16400ca01c
Andrea and Jeremy <jparris195@gmail.com>
2017-04-22 23:04:45 -0500

    auto mapped profile photo data

--- ParrisConnection.ServiceLayer/Services/ProfilePhotosService.cs
+++ ParrisConnection.ServiceLayer/Services/ProfilePhotosService.cs
@@ -1,4 +1,5 @@
-﻿using ParrisConnection.DataLayer.DataAccess;
+﻿using AutoMapper;
+using ParrisConnection.DataLayer.DataAccess;
 using ParrisConnection.DataLayer.Entities.Profile;
 using ParrisConnection.ServiceLayer.Data;
 using ParrisConnection.ServiceLayer.Services.Interfaces;
@@ -12,15 +13,20 @@ namespace ParrisConnection.ServiceLayer.Services
     public class ProfilePhotosService : IProfilePhotosService
     {
         private readonly IDataAccess _dataAccess;
+        private readonly IMapper _mapper;
 
         public ProfilePhotosService(IDataAccess dataAccess)
         {
             _dataAccess = dataAccess;
+
+            var config = new MapperConfiguration(m => m.CreateMap<ProfilePhoto, ProfilePhotoData>().ReverseMap());
+
+            _mapper = new Mapper(config);
         }
 
         public IEnumerable<ProfilePhotoData> GetAllProfilePhotos()
         {
-            return _dataAccess.ProfilePhotoes.GetAll().Select(LoadData);
+            return _mapper.Map<IEnumerable<ProfilePhotoData>>(_dataAccess.ProfilePhotoes.GetAll());
         }
 
         public ProfilePhotoData GetProfilePhoto(string userId)
@@ -28,7 +34,7 @@ namespace ParrisConnection.ServiceLayer.Services
             var photos = _dataAccess.ProfilePhotoes.GetAll().ToList();
             var photo = photos.SingleOrDefault(p => p.UserId == userId);
 
-            return photo != null ? LoadData(photo) : new ProfilePhotoData();
+            return photo != null ? _mapper.Map<ProfilePhotoData>(photo) : new ProfilePhotoData();
         }
 
         public void UpdateProfilePhoto(HttpPostedFileBase file, string filePath, string userId)
@@ -46,16 +52,6 @@ namespace ParrisConnection.ServiceLayer.Services
             file.SaveAs(filePath);
             
             _dataAccess.ProfilePhotoes.Insert(new ProfilePhoto {FilePath = file.FileName, UserId = userId});
-        }
-
-        private ProfilePhotoData LoadData(ProfilePhoto photo)
-        {
-            return new ProfilePhotoData
-            {
-                Id       = photo.Id,
-                UserId   = photo.UserId,
-                FilePath = photo.FilePath
-            };
         }
     }
 }
