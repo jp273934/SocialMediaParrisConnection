c1e7d91616af4bc8c495658388b214e65e98e8e8
Andrea and Jeremy <jparris195@gmail.com>
2017-04-22 23:10:07 -0500

    auto mapped quote data

--- ParrisConnection.ServiceLayer/Services/QuoteService.cs
+++ ParrisConnection.ServiceLayer/Services/QuoteService.cs
@@ -1,56 +1,39 @@
-﻿using ParrisConnection.DataLayer.DataAccess;
+﻿using AutoMapper;
+using ParrisConnection.DataLayer.DataAccess;
 using ParrisConnection.DataLayer.Entities.Profile;
 using ParrisConnection.ServiceLayer.Data;
 using ParrisConnection.ServiceLayer.Services.Interfaces;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace ParrisConnection.ServiceLayer.Services
 {
     public class QuoteService : IQuoteService
     {
         private readonly IDataAccess _dataAccess;
+        private readonly IMapper _mapper;
 
         public QuoteService(IDataAccess dataAccess)
         {
             _dataAccess = dataAccess;
+
+            var config = new MapperConfiguration(m => m.CreateMap<Quote, QuoteData>().ReverseMap());
+
+            _mapper = new Mapper(config);
         }
 
         public IEnumerable<QuoteData> GetQuotes()
         {
-            return _dataAccess.Quotes.GetAll().Select(LoadData);
+            return _mapper.Map<IEnumerable<QuoteData>>(_dataAccess.Quotes.GetAll());
         }
 
         public QuoteData GetQuoteById(int id)
         {
-            return LoadData(_dataAccess.Quotes.GetById(id));
+            return _mapper.Map<QuoteData>(_dataAccess.Quotes.GetById(id));
         }
 
         public void SaveQuote(QuoteData quote)
         {
-            _dataAccess.Quotes.Insert(ConvertToEntity(quote));
-        }
-
-        private QuoteData LoadData(Quote quote)
-        {
-            return new QuoteData
-            {
-                Id     = quote.Id,
-                UserId = quote.UserId,
-                Name   = quote.Name,
-                Author = quote.Author
-            };
-        }
-
-        private Quote ConvertToEntity(QuoteData quote)
-        {
-            return new Quote
-            {
-                Id     = quote.Id,
-                UserId = quote.UserId,
-                Name   = quote.Name,
-                Author = quote.Author
-            };
+            _dataAccess.Quotes.Insert(_mapper.Map<Quote>(quote));
         }
     }
 }
