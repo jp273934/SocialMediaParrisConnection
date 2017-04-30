496d4a1b84573b81f88db48b4c0e4e1d8dba3036
Andrea and Jeremy <jparris195@gmail.com>
2017-04-30 10:21:59 -0500

    Profile photo query service unit test

---
+++ ParrisConnection.Tests/Services/ProfilePhotoQueryServiceTest.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using ParrisConnection.DataLayer.DataAccess;
+using ParrisConnection.DataLayer.Entities.Profile;
+using ParrisConnection.DataLayer.Repositories;
+using ParrisConnection.ServiceLayer.Data;
+using ParrisConnection.ServiceLayer.Services.ProfilePhoto.Queries;
+using System.Collections.Generic;
+
+namespace ParrisConnection.Tests.Services
+{
+    [TestClass]
+    public class ProfilePhotoQueryServiceTest
+    {
+        private ProfilePhotoQueryService _profilePhotoQueryService;
+        private Mock<IDataAccess> _dataAccess;
+        private Mock<IRepository<ProfilePhoto>> _profilePhotoRepository;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _profilePhotoRepository = new Mock<IRepository<ProfilePhoto>>();
+            _dataAccess = new Mock<IDataAccess>();
+            _dataAccess.Setup(d => d.ProfilePhotoes).Returns(_profilePhotoRepository.Object);
+            _profilePhotoQueryService = new ProfilePhotoQueryService(_dataAccess.Object);
+        }
+
+        [TestMethod]
+        public void GetAllProfilePhotos_Should_Return_Ienumerable_ProfilePhotoData()
+        {
+            Assert.IsInstanceOfType(_profilePhotoQueryService.GetAllProfilePhotos(), typeof(IEnumerable<ProfilePhotoData>));
+        }
+
+        [TestMethod]
+        public void GetProfilePhoto_Should_Return_ProfilePhotoData()
+        {
+            Assert.IsInstanceOfType(_profilePhotoQueryService.GetProfilePhoto(""), typeof(ProfilePhotoData));
+        }
+    }
+}
